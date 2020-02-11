61b299ec212979c18414bf7ba20d8f200b80e64c
Kohvu <PJIPark@gmail.com>
2020-02-11 05:31:35 -0800

    Create SearchServiceTest.java

---
+++ SearchServiceTest.java
@@ -0,0 +1,11 @@
+
+public class SearchServiceTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SearchService test = new SearchService();
+		
+		System.out.println("ID: " + test.Search("Dimensions"));
+	}
+
+}
